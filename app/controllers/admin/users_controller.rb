609c908178cef57491a03c0727a1711cd83854e9
iammhardy <iamhardyusuf@gmail.com>
2026-03-25 07:13:08 +0100

    Updated the UI

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,44 +2,45 @@ class Admin::UsersController < Admin::BaseController
   before_action :set_company
   before_action :load_collections, only: [:new, :create]
 
- def index
-    @users = User.includes(:company, :sector).all
-  end
-  def new
-    @user = User.new
-    @user.company = @company if @company
+  def index
+    @users = @company.users.includes(:company, :sector)
   end
 
-  def create
+  def new
+    @user = @company.users.new
+  end
+
+ def create
   @user = @company.users.build(user_params)
 
-  respond_to do |format|
-    if @user.save
-      format.turbo_stream { render turbo_stream: turbo_stream.prepend("users_list", partial: "admin/users/user", locals: { user: @user }) }
-      format.html { redirect_to admin_company_users_path(@company), notice: "User created" }
-    else
-      format.turbo_stream { render turbo_stream: turbo_stream.replace("new_user_form", partial: "admin/users/form", locals: { user: @user, company: @company, sectors: @sectors, managers: @managers }) }
-      format.html { render :new, status: :unprocessable_entity }
-    end
+  if @user.save
+    redirect_to admin_company_users_path(@company), notice: "User created successfully."
+  else
+    flash.now[:alert] = "Please fix the errors below."
+    render :new, status: :unprocessable_entity
   end
 end
 
   private
 
-  def load_collections
-  @companies = Company.all
-  @company = Company.find_by(id: params[:company_id])
-  @sectors = @company ? @company.sectors : Sector.all
-  @managers = @company ? @company.users.where(role: :manager) : User.where(role: :manager)
-end
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :role, :company_id, :sector_id, :manager_id)
+  def set_company
+    @company = Company.find(params[:company_id])
   end
 
-  def set_company
-  @company = Company.find(params[:company_id])
-end
+  def load_collections
+    @sectors = @company.sectors
+    @managers = @company.users.where(role: :manager)
+  end
 
- 
+  def user_params
+    params.require(:user).permit(
+      :name,
+      :email,
+      :password,
+      :password_confirmation,
+      :role,
+      :sector_id,
+      :manager_id
+    )
+  end
 end
